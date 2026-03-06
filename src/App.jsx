04290dabcceba90e4ff12207b9d2af94e478d80c
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-03-06 13:12:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import AppHeader from "./components/AppHeader"
+import AppMain from "./components/AppMain"
+import AppFooter from "./components/AppFooter"
+/* import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg' */
+
+
+function App() {
+
+  return (
+    <>
+      <AppHeader />
+      <AppMain />
+      <AppFooter />
+    </>
+  )
+}
+
+export default App
