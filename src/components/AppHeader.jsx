046dcc864c7484024c31053d1775a942ee08bb19
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-03-06 14:57:33 +0100

    add AppHeader

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -3,7 +3,23 @@ export default function AppHeader() {
 
     return (
         <header>
-            Header
+
+            <img src="/img/dc-logo.png" alt="" className="header-logo" />
+            <nav className="navbar" id="navbar">
+
+
+                <a href="">CHARACTERS</a>
+                <a href="" className='active'>COMICS</a>
+                <a href="">MOVIES</a>
+                <a href="">TV</a>
+                <a href="">GAMES</a>
+                <a href="">COLLECTIBLES</a>
+                <a href="">VIDEOS</a>
+                <a href="">FANS</a>
+                <a href="">NEWS</a>
+                <a href="">SHOP</a>
+
+            </nav>
         </header>
     )
 }
