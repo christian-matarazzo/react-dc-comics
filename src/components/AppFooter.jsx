f4e71f77b3d98c67f6e1e76d7e951c8b2360a610
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-03-07 13:21:12 +0100

    add footer jumbo

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -8,19 +8,62 @@ import youtube from "../assets/img/footer-youtube.png"
 export default function AppFooter() {
 
     return (
-        <footer>
-            <div className="background-images">
-                <img src={backGround} alt="background" srcset="" id="background-footer" />
-                <img src={dcLogo} alt="" srcset="" id="dc-logo" />
+        <footer className="main-footer">
+            <div className="footer-bg-wrapper">
+                <img src={backGround} alt="background" className="bg-img" />
+                <img src={dcLogo} alt="DC Logo" className="logo-overlay" />
             </div>
 
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <h4>Ciao</h4>
+
+            <div className="container footer-content">
+                <div className="row gap-5">
+                    <div className="col-auto">
+                        <h3>DC COMICS</h3>
+                        <ul>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+                        <h3>SHOP</h3>
+                        <ul>
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
+                        </ul>
+                    </div>
+
+
+                    <div className="col-auto">
+                        <h3>DC</h3>
+                        <ul>
+                            <li>Terms Of Use</li>
+                            <li>Privacy policy (New)</li>
+                            <li>Ad Choices</li>
+                            <li>Advertising</li>
+                            <li>Jobs</li>
+                            <li>Subscription</li>
+                            <li>Talent Workshops</li>
+                            <li>CPSC Certificates</li>
+                            <li>Ratings</li>
+                            <li>Shop Help</li>
+                            <li>Contact us</li>
+                        </ul>
+                    </div>
+                    <div className="col-auto">
+                        <h3>SITES</h3>
+                        <ul>
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Kids</li>
+                            <li>DC Universe</li>
+                            <li>DC Power Visa</li>
+                        </ul>
                     </div>
                 </div>
             </div>
         </footer>
-    )
+    );
 }
