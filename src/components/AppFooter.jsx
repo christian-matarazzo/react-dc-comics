d263d9b35107f5f093935a5f65c30e7ec2892d1f
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-03-09 16:07:35 +0100

    add import and export instead of array

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -5,42 +5,46 @@ import periscote from "../assets/img/footer-periscope.png"
 import pinterest from "../assets/img/footer-pinterest.png"
 import twitter from "../assets/img/footer-twitter.png"
 import youtube from "../assets/img/footer-youtube.png"
+import { dcComics } from "../../JS-array/footer"
+import { dcShop } from "../../JS-array/footer"
+import { dcMain } from "../../JS-array/footer"
+import { dcSites } from "../../JS-array/footer"
 export default function AppFooter() {
-    const dcComics = [
-        { id: 1, text: "Characters" },
-        { id: 2, text: "Comics" },
-        { id: 3, text: "Movies" },
-        { id: 4, text: "TV" },
-        { id: 5, text: "Games" },
-        { id: 6, text: "Videos" },
-        { id: 7, text: "News" },
-    ]
-    const dcShop = [
-        { id: 1, text: "Shop DC" },
-        { id: 2, text: "Shop DC Collectables" }
-    ]
-    const dcMain = [
-        { id: 1, text: "Terms Of Use" },
-        { id: 2, text: "Privacy policy (New)" },
-        { id: 3, text: "Ad Choices" },
-        { id: 4, text: "Advertising" },
-        { id: 5, text: "Jobs" },
-        { id: 6, text: "Subscription" },
-        { id: 7, text: "Talent Workshops" },
-        { id: 8, text: "CPSC Certificates" },
-        { id: 9, text: "Ratings" },
-        { id: 10, text: "Shop Help" },
-        { id: 11, text: "Contact us" },
-
-
-    ]
-    const dcSites = [
-        { id: 1, text: "DC" },
-        { id: 2, text: "MAD Magazine" },
-        { id: 3, text: "DC Kids" },
-        { id: 4, text: "DC Universe" },
-        { id: 5, text: "DC Power Visa" },
-    ]
+    /*     const dcComics = [
+            { id: 1, text: "Characters" },
+            { id: 2, text: "Comics" },
+            { id: 3, text: "Movies" },
+            { id: 4, text: "TV" },
+            { id: 5, text: "Games" },
+            { id: 6, text: "Videos" },
+            { id: 7, text: "News" },
+        ] */
+    /*     const dcShop = [
+            { id: 1, text: "Shop DC" },
+            { id: 2, text: "Shop DC Collectables" }
+        ] */
+    /*     const dcMain = [
+            { id: 1, text: "Terms Of Use" },
+            { id: 2, text: "Privacy policy (New)" },
+            { id: 3, text: "Ad Choices" },
+            { id: 4, text: "Advertising" },
+            { id: 5, text: "Jobs" },
+            { id: 6, text: "Subscription" },
+            { id: 7, text: "Talent Workshops" },
+            { id: 8, text: "CPSC Certificates" },
+            { id: 9, text: "Ratings" },
+            { id: 10, text: "Shop Help" },
+            { id: 11, text: "Contact us" },
+    
+    
+        ] */
+    /*     const dcSites = [
+            { id: 1, text: "DC" },
+            { id: 2, text: "MAD Magazine" },
+            { id: 3, text: "DC Kids" },
+            { id: 4, text: "DC Universe" },
+            { id: 5, text: "DC Power Visa" },
+        ] */
 
     return (
         <footer className="footer-wrapper">
